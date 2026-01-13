f209e23a269238ea2cdc6f1900ec4ece837d8913
bobzeee <edegboewoma@gmail.com>
2026-01-13 15:11:19 +0100

    Setup Auth Layouts, Social Auth Form, Next Auth,OAuth with Google and Github

---
+++ app/(root)/page.tsx
@@ -0,0 +1,25 @@
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+const Home = async () => {
+  const session = await auth();
+
+  return (
+    <>
+      <h1 className="text-3xl">WElcome</h1>
+      <h1 className="font-space-grotesk text-3xl">WElcome gg</h1>
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Log Out</Button>
+      </form>
+    </>
+  );
+};
+
+export default Home;
---
+++ components/forms/SocialAuthForm.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React from "react";
+import { Button } from "../ui/button";
+import Image from "next/image";
+import { toast } from "sonner";
+import { signIn } from "next-auth/react";
+import ROUTES from "@/constants/routes";
+
+const SocialAuthForm = () => {
+  const buttonClassNames =
+    "background-dark400_light900 body-medium text-dark200_light800 rounded-2 min-h-12 flex-1 px-2 py-3.5";
+
+  const handleSignIn = async (provider: "github" | "google") => {
+    try {
+      await signIn(provider, {
+        callbackUrl: ROUTES.HOME,
+        redirect: true,
+      });
+    } catch (error) {
+      console.error(`Error during ${provider} sign-in:`, error);
+      toast.error("Sign-In Failed", {
+        description: error instanceof Error ? error.message : "An error occured during sign-in",
+      });
+    }
+  };
+
+  return (
+    <div className="mt-10 flex flex-wrap gap-2.5">
+      <Button className={buttonClassNames} onClick={() => handleSignIn("github")}>
+        <Image
+          src="/icons/github.svg"
+          alt="GitHub"
+          width={20}
+          height={20}
+          className="invert-colors mr-2.5 object-contain"
+        />
+        <span>Log in with GitHub</span>
+      </Button>
+      <Button className={buttonClassNames} onClick={() => handleSignIn("google")}>
+        <Image src="/icons/google.svg" alt="Google" width={20} height={20} className="mr-2.5 object-contain" />
+        <span>Log in with Google</span>
+      </Button>
+    </div>
+  );
+};
+
+export default SocialAuthForm;
