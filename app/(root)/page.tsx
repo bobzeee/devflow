5f6c4d895e148d19612eab8a251a59a4599b74d0
bobzeee <edegboewoma@gmail.com>
2026-01-15 14:38:38 +0100

    Mobile Navigation and NavLinks

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,4 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+import { auth } from "@/auth";
 
 const Home = async () => {
   const session = await auth();
@@ -9,15 +7,6 @@ const Home = async () => {
     <>
       <h1 className="text-3xl">WElcome</h1>
       <h1 className="font-space-grotesk text-3xl">WElcome gg</h1>
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Log Out</Button>
-      </form>
     </>
   );
 };
