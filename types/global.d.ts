2783e49a5dca90d5d821fd5c4463da8409ce6cbe
bobzeee <edegboewoma@gmail.com>
2026-01-19 21:50:36 +0100

    Implemented QuestionCard

---
+++ types/global.d.ts
@@ -0,0 +1,23 @@
+interface Tag {
+  _id: string;
+  name: string;
+}
+
+interface Author {
+  _id: string;
+  name: string;
+  image?: string;
+}
+
+interface Question {
+  question: {
+    _id: string;
+    title: string;
+    tags: Tag[];
+    author: Author;
+    upvotes: number;
+    answers: number;
+    views: number;
+    createdAt: Date;
+  };
+}
