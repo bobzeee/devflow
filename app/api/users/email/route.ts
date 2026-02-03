99bdbd3482d25f0b65cb67abdba5da6dad53df98
bobzeee <edegboewoma@gmail.com>
2026-02-03 11:23:27 +0100

    Implemented dynamic User routes and User by email

---
+++ app/api/users/email/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+
+import User from "@/database/user.model";
+import handleError from "@/lib/handlers/error";
+import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import { UserSchema } from "@/lib/validations";
+import z from "zod";
+
+export async function POST(request: Request) {
+  const { email } = await request.json();
+
+  try {
+    const validatedData = UserSchema.partial().safeParse({ email });
+
+    if (!validatedData.success) {
+      const flattened = z.flattenError(validatedData.error);
+      throw new ValidationError(flattened.fieldErrors);
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) throw new NotFoundError("User");
+
+    return NextResponse.json(
+      {
+        success: true,
+        data: user,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
